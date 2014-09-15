60627c2b33f3a8fd9311551e0837629f8fbab2f3
Sascuash <whitedragongroup@gmail.com>
2014-09-15 20:53:13 +0200

    footer.php

--- alquiler.php
+++ alquiler.php
@@ -1,6 +1,4 @@
 <? include ("header.php"); ?>
 
-	<body>
  		<?php echo '<p>Query para ver videoclubs registrados</p>'; ?>
-	</body>
-</html>
+<? include ("footer.php"); ?>
--- alta.php
+++ alta.php
@@ -1,5 +1,4 @@
 <? include ("header.php"); ?>
 
  		<?php echo '<p>Query para ver videoclubs registrados</p>'; ?>
-	</body>
-</html>
+<? include ("footer.php"); ?>

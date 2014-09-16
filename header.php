736e61f359b8a8ebd75e0fe25405cf0af2626f1c
Sascuash <whitedragongroup@gmail.com>
2014-09-16 09:07:48 +0200

    Añadir boostrap css en header.php

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <html>
 	<head>
  		<title>Selección de videoclub</title>
+ 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+      	<!-- Bootstrap -->
+      	<link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
 	</head>
 	<body>
 		<h1>Videoclub</h1>

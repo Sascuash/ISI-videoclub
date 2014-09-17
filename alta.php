2858ab60c553913c3746d3d9b22630654a048048
Sascuash <whitedragongroup@gmail.com>
2014-09-17 15:23:48 +0200

    Add usuario & add php al codigo

--- alta.php
+++ alta.php
@@ -1,7 +1,27 @@
-<? include ("header.php"); ?>
+<?php
+	include ("php/classVideo.php");
+	$video=new classVideo();
 
+	if ($_POST["btnEnviar"]=="Guardar") {
+		$socio=$_POST["txtSocio"];
+		$edad=$_POST["txtEdad"];
+		
+		$idSocioInsertado=$video->insertaSocio($socio, $edad, $idVideoclub, 0, "", "");
+
+	}
+?>
+<?php include ("header.php"); ?>
+		<?php
+			if ($idSocioInsertado>0) {
+				?>
+				<script type="text/javascript">
+				alert("Se ha creado un socio con id: <?php echo $idSocioInsertado; ?>");
+				</script>
+				<?php
+			}
+		?>
  		<h2>Alta de Socios</h2>
- 		<form>
+ 		<form method="post" action="">
  			<p>
  				<label>Nombre</label>
  				<input type="text" name="txtSocio" value="" id="txtSocio"/>
@@ -16,4 +36,4 @@
  			</p>
  		</form>
 
-<? include ("footer.php"); ?>
+<?php include ("footer.php"); ?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<? include ("header.php"); ?>
+<?php include ("header.php"); ?>
 
  		<?php echo '<p>Query para ver videoclubs registrados</p>'; ?>
-<? include ("footer.php"); ?>
+<?php include ("footer.php"); ?>

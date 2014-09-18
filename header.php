fad7002b045f36f1d24be2720200bdb835b55480
Sascuash <whitedragongroup@gmail.com>
2014-09-18 18:37:39 +0200

    Ajustar css

--- header.php
+++ header.php
@@ -4,17 +4,18 @@
  		<meta name="viewport" content="width=device-width, initial-scale=1.0">
       	<!-- Bootstrap -->
       	<link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+      	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
       	<nav class="navbar navbar-default" role="navigation">
 		  <div class="container-fluid">
 		    <!-- Brand and toggle get grouped for better mobile display -->
-		    <div class="navbar-header">
+		    <div class="navbar-header" style="margin-top:14px;">
 			    <font size=6>Videoclub</font>&emsp;
 		    </div>
 		
 		    <!-- Collect the nav links, forms, and other content for toggling -->
 		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		      <ul class="nav navbar-nav">
+		      <ul class="nav navbar-nav" style="margin-top:4px;">
 		        <li class="active"><a href="index.php">Inicio</a></li>
 				<li><a href="login.php">Login</a></li>
 				<li class="active"><a href="alta.php">Alta</a></li>
@@ -27,12 +28,17 @@
 
 	</head>
 	<body style="margin-left:5%; margin-right:5%;">
-		<?php
-			if (isset($_SESSION["idSocio"])) {
-				echo '<p>Socio actual: '.$_SESSION["idSocio"].'</p>';
-			}
-			if (isset($_SESSION["idVideoclub"])) {
-				echo '<p>Videoclub actual: '.$_SESSION["idVideoclub"].'</p>';
-			}
-		?>
+		<div class="row">
+			<?php
+			    echo '<p>';
+				if (isset($_SESSION["idSocio"])) {
+					echo 'Socio actual: '.$_SESSION["idSocio"].' ';
+				}
+				echo ' ';
+				if (isset($_SESSION["idVideoclub"])) {
+					echo 'Videoclub actual: '.$_SESSION["idVideoclub"].' ';
+				}
+				echo '</p>';
+			?>
+		</div>
 		
